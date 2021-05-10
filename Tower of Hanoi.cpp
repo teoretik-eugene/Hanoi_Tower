0c0d58a9ea7608b967724ded5637daabc745ebdb
teoretik-eugene <74543647+teoretik-eugene@users.noreply.github.com>
2021-05-10 15:34:37 +0300

    Update Tower of Hanoi.cpp

--- Tower of Hanoi.cpp
+++ Tower of Hanoi.cpp
@@ -54,11 +54,10 @@ public:
 
 /* Класс стека */
 class Stack {
-    int max_size = 8;     // Максимальное кол-во элементов в стеке
     Disk* tos_ = nullptr;
-    int count_;
 
 public:
+    int count_;
     Stack() {
         count_ = 0;
     };
@@ -108,12 +107,15 @@ class Game {
 public:
     int x_ = 12;            // Координаты начала игры
     int y_;
+    int size_;
+    Stack* new_tower;
 
     Disk* d_ptr = nullptr;
     Stack* s_ptr = nullptr;
     Game(Stack *steck1) {
-        s_ptr = steck1;
+        s_ptr = steck1;         // Делаем так, чтобы игра начиналась с первой пирамидки
         DrawPointer(x_);
+        size_ = s_ptr->count_;
     };
 
     void Move(Disk *disk_ch, int x, int y) {          // disk_ch - указатель на выбранный диск
@@ -140,25 +142,26 @@ public:
 
     }
 
-    /* Указатель на выбранную башню */
+    /* Создаем стрелочку, указывающая на выбранную башню */
     void DrawPointer(int x, int y = 17) {
         char ptr = 30;
         console_gotoxy(x, y);
         std::cout << ptr;
     }
-
+        /* Убираем стрелочку */
     void RemovePointer(int x, int y = 17) {
         console_gotoxy(x, y);
         std::cout << ' ';
     }
 
     void Start(Stack *steck1, Stack *steck2, Stack *steck3) {
+        new_tower = steck3;
 
         console_hide_cursor();
         WORD vk;
         bool isFinish = false;
-        bool isUp = false;
-        bool isDown = true;
+        bool isUp = false;          // isUp означает, что диск подняли и его можно только опустить или перенести
+        bool isDown = true;         // isUp - опустили 
 
         while (!isFinish) {
             if (_kbhit()) {
@@ -189,14 +192,14 @@ public:
                 case VK_DOWN:
                     if (isDown)
                         break;
-
+                    /* Опускаем вниз диск */
                     if (s_ptr != nullptr && d_ptr != nullptr) {
                         Disk *prev_disk = s_ptr->peek();
                         if (prev_disk == nullptr) {
                             int len = d_ptr->length_;
                             int x_new = d_ptr->x_; int y_new = d_ptr->y_;
                             ReMove(d_ptr, x_, y_);
-                            s_ptr->push(len, x_, 15);       // Опускаем в самый низ
+                            s_ptr->push(len, x_, 15);       
                             //s_ptr = nullptr;
                             d_ptr = nullptr;
                             isDown = true;
@@ -213,7 +216,7 @@ public:
                                 //std::cout << "DOWN";
                                 int x_new = prev_disk->x_; int y_new = prev_disk->y_;
                                 ReMove(d_ptr, x_, y_);
-                                x_ = x_new; y_ = y_new - 1;             // говницо получается
+                                x_ = x_new; y_ = y_new - 1;             
                                 Move(d_ptr, x_, y_);
                                 s_ptr->push(len, x_, y_);
                                 d_ptr = nullptr;
@@ -305,14 +308,17 @@ public:
                     console_gotoxy(12, 4);
                     std::cout << " \t\t\t\tGAME OVER ";
                     isFinish = true;
-                    break;
+                break;
                 }
+            }
+            int new_size = new_tower->count_;
+            if (size_ == new_size) {
+                isFinish = true;
+                console_gotoxy(12, 4);
+                std::cout << " \t\t\t\tYOU WIN ";
             }
         } 
     }
-    
-
-
 };
 
 int main()
